7f770843b83b91dc18676ed7267518b40f229bf5
Theodor Samson <61683286+Theo3699@users.noreply.github.com>
2020-12-07 13:56:15 +0200

    Added the main code under a function

--- project.py
+++ project.py
@@ -4,22 +4,26 @@ import pathlib
 def percentage(part, whole):
     return 100 * float(part)/float(whole)
 
-countDirs=0
-countFiles=0
-myDict=dict()
-for root, dirs, files in os.walk("D:\\"):
-    for directory in dirs:
-        countDirs+=1
-    for filenames in files:
-        countFiles+=1
-        extension=pathlib.Path(filenames).suffix
-        if not extension:
-            print(os.path.abspath(filenames))
-            print(filenames)
-        if extension in myDict:
-            myDict[extension]+=1
-        else:
-            myDict[extension]=1
-print("Exista ", countDirs, " directoare si ", countFiles, " fisiere")
-for keys in myDict:
-    print(keys, percentage(myDict[keys], countFiles), "%")
+
+def statistics(path):
+    countDirs=0
+    countFiles=0
+    myDict=dict()
+    for root, dirs, files in os.walk(path):
+        for directory in dirs:
+            countDirs+=1
+        for filenames in files:
+            countFiles+=1
+            extension=pathlib.Path(filenames).suffix
+            if not extension:
+                print(os.path.abspath(filenames))
+                print(filenames)
+            if extension in myDict:
+                myDict[extension]+=1
+            else:
+                myDict[extension]=1
+    print("Exista ", countDirs, " directoare si ", countFiles, " fisiere")
+    for keys in myDict:
+        print(keys, percentage(myDict[keys], countFiles), "%")
+
+statistics("D:\\")
