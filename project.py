023dae50b319b8813ac61d09f0787fb6a06a9eba
Theodor Samson <61683286+Theo3699@users.noreply.github.com>
2020-12-07 13:28:02 +0200

    Afiseaza in consola procentajele
    Cred ca exista un bug? gaseste foarte multe fisiere care nu au extensie, dar pe care nu le gasesc pe partitii

--- project.py
+++ project.py
@@ -1,12 +1,25 @@
 import os
+import pathlib
+
+def percentage(part, whole):
+    return 100 * float(part)/float(whole)
 
 countDirs=0
 countFiles=0
+myDict=dict()
 for root, dirs, files in os.walk("D:\\"):
     for directory in dirs:
-        print(directory)
         countDirs+=1
     for filenames in files:
-        print(filenames)
         countFiles+=1
+        extension=pathlib.Path(filenames).suffix
+        if not extension:
+            print(os.path.abspath(filenames))
+            print(filenames)
+        if extension in myDict:
+            myDict[extension]+=1
+        else:
+            myDict[extension]=1
 print("Exista ", countDirs, " directoare si ", countFiles, " fisiere")
+for keys in myDict:
+    print(keys, percentage(myDict[keys], countFiles), "%")
