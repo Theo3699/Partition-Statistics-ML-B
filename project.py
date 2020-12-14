e3a8ffff95980ea5420572eb230ccd3f4239b068
Theodor Samson <61683286+Theo3699@users.noreply.github.com>
2020-12-14 13:54:45 +0200

    Update project.py
    Added a dialog box, matplotlib and the percentages in the representation

--- project.py
+++ project.py
@@ -1,5 +1,8 @@
 import os
 import pathlib
+import wx
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import matplotlib.pyplot as plt
 
 def percentage(part, whole):
     return 100 * float(part)/float(whole)
@@ -9,21 +12,42 @@ def statistics(path):
     countDirs=0
     countFiles=0
     myDict=dict()
-    for root, dirs, files in os.walk(path):
+    print(path)
+    os.chdir(path)
+    for root, dirs, files in os.walk("."):
+        print(root)
         for directory in dirs:
             countDirs+=1
         for filenames in files:
             countFiles+=1
             extension=pathlib.Path(filenames).suffix
-            if not extension:
-                print(os.path.abspath(filenames))
+            if extension:
                 print(filenames)
-            if extension in myDict:
-                myDict[extension]+=1
-            else:
-                myDict[extension]=1
-    print("Exista ", countDirs, " directoare si ", countFiles, " fisiere")
-    for keys in myDict:
-        print(keys, percentage(myDict[keys], countFiles), "%")
+                if extension in myDict:
+                    myDict[extension]+=1
+                else:
+                    myDict[extension]=1
+    print("Exista ", countDirs, " directoare si ", countFiles, " fisiere in ", path)
+    sortedDict=dict()
+    count=0
+    for keys in sorted(myDict, key=myDict.get, reverse=True):
+        if (count<10):
+            sortedDict[keys]=percentage(myDict[keys], countFiles)
+            count+=1
+    print("Aici incepe dictionarul sortat")
+    percs = list()
+    for keys in sortedDict:
+        print(keys, sortedDict[keys], "%")
+        percs.append(sortedDict[keys])
+    fig,ax=plt.subplots(1, 1)
+    ax.bar(sortedDict.keys(), percs)
+    plt.ylabel("procentaje")
+    plt.xlabel("extensii")
+    plt.title("statistica")
+    plt.show()
 
-statistics("D:\\")
+app = wx.PySimpleApp()
+dialog = wx.DirDialog(None, "Choose a directory:",style=wx.DD_DEFAULT_STYLE | wx.DD_NEW_DIR_BUTTON)
+if dialog.ShowModal() == wx.ID_OK:
+    statistics(dialog.GetPath())
+dialog.Destroy()
